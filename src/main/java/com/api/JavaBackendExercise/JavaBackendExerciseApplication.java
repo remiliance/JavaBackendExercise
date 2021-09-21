b3c31fcd6e4d7924bc1092470f828d6427307f5a
Remil <remiliance@yahoo.fr>
2021-09-21 10:12:34 +0200

    init du projet Spring Boot

---
+++ src/main/java/com/api/JavaBackendExercise/JavaBackendExerciseApplication.java
@@ -0,0 +1,13 @@
+package com.api.JavaBackendExercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBackendExerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaBackendExerciseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/JavaBackendExercise/JavaBackendExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.JavaBackendExercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaBackendExerciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
